6c22915d4508afe0d4aee99b9fa03eb9919f8811
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-22 12:16:34 +0900

    Update ［1차］ 비밀지도.py

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,35 +1,11 @@
 def solution(n, arr1, arr2):
     answer = []
     
-    temp_list_arr1 = []
-    temp_list_arr2 = []
+    temp_list_arr1 = [change2binary(num, n) for num in arr1]
+    temp_list_arr2 = [change2binary(num, n) for num in arr2]
 
-    for arr1_num in arr1:
-        arr_binary_1 = change2binary(arr1_num, n)
-        temp_list_arr1.append(arr_binary_1)
-
-    for arr2_num in arr2:
-        arr_binary_2 = change2binary(arr2_num, n)
-        temp_list_arr2.append(arr_binary_2)
-
-
-    for i in range(len(temp_list_arr1)):
-        temp_str = ''
-        for j in temp_list_arr1[i]:
-            if j == '0':
-                temp_str += ' '
-            else:
-                temp_str += '#'
-        temp_list_arr1[i] = temp_str
-
-    for i in range(len(temp_list_arr2)):
-        temp_str = ''
-        for j in temp_list_arr2[i]:
-            if j == '0':
-                temp_str += ' '
-            else:
-                temp_str += '#'
-        temp_list_arr2[i] = temp_str
+    temp_list_arr1 = [''.join(' ' if bit == '0' else '#' for bit in binary_str) for binary_str in temp_list_arr1]
+    temp_list_arr2 = [''.join(' ' if bit == '0' else '#' for bit in binary_str) for binary_str in temp_list_arr2]
 
     for i in range(n):
         temp = ''
