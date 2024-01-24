36e7839c5a7260744bf88428487a9c0db8f0c8f8
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-24 10:35:36 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,13 @@
+def solution(cards1, cards2, goals):
+    card1_index = 0
+    card2_index = 0
+    for goal in goals:
+
+        if len(cards1) > card1_index and goal == cards1[card1_index]:
+            card1_index += 1
+        elif len(cards2) > card2_index and goal == cards2[card2_index]:
+            card2_index += 1
+        else:
+            return "No"
+
+    return "Yes"
