2f5f5c191599346d117863b417f44729d7813e59
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-25 16:19:18 +0900

    [level 1] Title: 기사단원의 무기, Time: 3.35 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,21 @@
+def num_divisor(a):
+    count = 0
+    for i in range(1, int(a**(1/2)) + 1):
+        if a % i == 0:
+            count+=1
+            if i**2 != a:
+                count += 1
+    return count
+
+
+def solution(number, limit, power):
+    answer = 0
+
+    for i in range(1,number+1):
+        temp = num_divisor(i)
+        if temp <= limit:
+            answer += temp
+        else:
+            answer += power
+
+    return answer
