a0ee81f80c19f20a13dd220944acdd273f48db34
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-25 13:59:42 +0900

    [level 1] Title: 모의고사, Time: 5.78 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,18 @@
+def solution(answers):
+    one = [1,2,3,4,5]
+    two = [2,1,2,3,2,4,2,5]
+    three = [3,3,1,1,2,2,4,4,5,5]
+
+    count = [0, 0, 0]
+
+    for i in range(len(answers)):
+        if answers[i%len(answers)] == one[i%len(one)]:
+            count[0] += 1
+        
+        if answers[i%len(answers)] == two[i%len(two)]:
+            count[1] += 1
+
+        if answers[i%len(answers)] == three[i%len(three)]:
+            count[2] += 1
+
+    return [index + 1 for index, num in enumerate(count) if num == max(count)]
