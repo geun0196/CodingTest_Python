2a59975466b895614af4321f11a8b1ef0f8ff46a
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-26 12:29:14 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -0,0 +1,20 @@
+def solution(N, stages):
+    answer = []
+    
+    length_stage = len(stages)
+    for i in range(1, N+1):
+        #print(length_stage)
+        if length_stage > 0: 
+            fail = stages.count(i) / length_stage
+            length_stage -= stages.count(i)
+            answer.append((i, fail))
+        else:
+            answer.append((i,0))
+
+        #print("%d번 스테이지 실패율: "%i, fail)
+
+    #print(answer)
+    answer.sort(key=lambda x: x[1], reverse=True)
+    answer = [x[0] for x in answer]
+
+    return answer
