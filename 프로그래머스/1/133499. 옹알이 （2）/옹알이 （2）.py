1a5ad490a80cbfbe341185ddd6d39caf90720d61
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-29 10:40:16 +0900

    [level 1] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,14 @@
+def solution(babbling):
+    answer = 0
+    can = ["aya", "ye", "woo", "ma"]
+
+    for bab in babbling:
+        for c in can:
+            if c * 2 not in bab: 
+                bab = bab.replace(c, ' ')
+            
+            if bab.isspace():
+                answer+=1
+                break
+            
+    return answer
