591533f227ec1aec316472ccd7926c228fdbd6be
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-30 14:17:28 +0900

    [level 1] Title: 문자열 나누기, Time: 0.87 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,24 @@
+def solution(s):
+    answer = 0
+
+    is_x = True
+    same_cnt = 0
+    diff_cnt = 0
+    for char in s:
+        if is_x:
+            x = char
+            is_x = False
+            answer += 1
+        
+        if char == x:
+            same_cnt += 1
+        elif char != x:
+            diff_cnt += 1
+
+        if same_cnt == diff_cnt:
+            
+            is_x = True
+            same_cnt = 0
+            diff_cnt = 0 
+
+    return answer
