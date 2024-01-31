7f84cdbe85978b6958eaef9c83663caba4b0c851
정승진 <73518321+geun0196@users.noreply.github.com>
2024-01-31 13:45:46 +0900

    [level 1] Title: 둘만의 암호, Time: 0.30 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,18 @@
+def solution(s, skip, index):
+    answer = ''
+    origin_index = index
+    
+    for char in s:
+        ord_char = ord(char)
+
+        while index > 0:
+            ord_char = (ord_char - ord('a') + 1) % 26 + ord('a')
+            if chr(ord_char) in skip:
+                index += 1
+            index -= 1
+
+        after_char = chr(ord_char)
+        answer += after_char
+        index = origin_index
+        
+    return answer
